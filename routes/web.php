2a8f3d5ceee653a8d5a9511cd6677886934d1caf
Pedro Vitor ferreira sena <pedrocurvelomg@gmail.com>
2023-04-03 12:35:55 -0300

    colocando e definindo tipos de parâmetros em uma rota

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,15 @@ Route::get('/', [PrincipalController::class, 'principal']);
 Route::get('/contato', [ContatoController::class, 'contato']);
 
 Route::get('/sobrenos', [SobreNosController::class, 'sobrenos']);
+
+Route::get(
+    '/contato/{nome}/{categoria_id}', 
+    function(
+        string $nome = 'Desconhecido',
+        int $categoria_id = 1
+    ){
+        echo "Estamos aqui: $nome - $categoria_id";
+    }
+)
+->where('categoria_id', '[0-9]+') //passar o parâmetro de categoria como sendo números entre 0 e 9
+->where('nome', '[A-Za-z]+') ; //passar o parâmetro de nome que seja apenas letras
