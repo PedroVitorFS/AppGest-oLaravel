8ef35d25add75080f245e15bceb4f5b4cef595af
Pedro Vitor ferreira sena <pedrocurvelomg@gmail.com>
2023-04-05 14:40:52 -0300

    estilização das páginas

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -13,3 +13,21 @@
 
     echo 'texto teste'
 @endphp
+
+{{-- 
+    O php identifica uma variável sendo empty como:
+    string = '' ou '0'
+    int = 0
+    float = 0.0
+    null
+    boolean = false
+    array = []
+--}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif (count($fornecedores)> 10)
+    <h3>Existem vários fornecedores cadastrados</h3> 
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
